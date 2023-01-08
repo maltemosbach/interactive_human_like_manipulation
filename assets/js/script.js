f3a44bb3513d768477eb44d3c15d17227be31302
Malte Mosbach <maltemosbach@gmail.com>
2023-01-08 20:28:32 +0100

    Update project page

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let papertitle = "Accelerating Interactive Human-like Manipulation Learning with GPU-based Simulation and High-quality Demonstrations"
+
+// set papertitle
+document.getElementById("papertitle_website").innerHTML = papertitle;
+document.getElementById("papertitle_heading").innerHTML = papertitle;
